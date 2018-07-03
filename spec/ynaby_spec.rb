d8842fef9a05d80eb21721afd87a599e2010edbb
Rafael Millan <rafmillan@gmail.com>
2018-07-03 21:33:42 +0200

    Initial commit

---
+++ spec/ynaby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ynaby do
+  it "has a version number" do
+    expect(Ynaby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
